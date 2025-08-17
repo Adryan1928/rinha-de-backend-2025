94deb5b92f425b787ff3ac5b48c60e8aafb8f4c8
Adryan1928 <adryanoliveira1928@gmail.com>
2025-08-17 14:29:27 -0300

    Atualizei as variáveis de ambiente para poder subir para o hub

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 import redis.asyncio as redis
 import os
 
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = os.getenv("REDIS_PORT", "6379")
 REDIS_DB = os.getenv("REDIS_DB", "0")
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True, max_connections=1000)
